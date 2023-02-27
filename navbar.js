927e1241abe5d3078b4e9b4aa7aeea70af1899ce
lanetb <lanetb@dukes.jmu.edu>
2023-02-26 19:17:48 -0500

    javascript not working

--- navbar.js
+++ navbar.js
@@ -1,20 +1,22 @@
 const nav = document.querySelector('nav');
-
-
-window.addEventListener('scroll', function() {
-    if (window.scrollY > 300) {
-        nav.classList.add('nav-scroll');
-    } else if (window.scrollY <= 300){
-        nav.classList.remove('nav-scroll');
-    }
-});
-
 const toggle = document.querySelector('.toggle-btn');
 const toggleIcon = document.querySelector('.toggle-btn i');
 const dropDown = document.querySelector('.drop-down');
 
+window.addEventListener('scroll', function() {
+    if (window.scrollY > 300) {
+        nav.classList.add('nav-scroll');
+        dropDown.style.setProperty('background','rgba(0, 0, 0, .15);');
+    } else if (window.scrollY <= 300){
+        nav.classList.remove('nav-scroll');
+        dropDown.style.setProperty('background','rgba(255, 255, 255, .15);');
+    }
+});
+
+
+
 toggle.onClick = function() {
-    dropDown.classList.toggle('.open');
+    dropDown.classList.add('open');
     const isOpen = dropDown.classList.contains('open');
 
     toggleIcon.classList = isOpen ? 'fas fa-xmark' : 'fas fa-bars';
