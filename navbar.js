24d91bad20b42845acef5ae943a136c63bdcc9d2
lanetb <lanetb@dukes.jmu.edu>
2023-02-26 20:27:26 -0500

    fixed javascript

--- navbar.js
+++ navbar.js
@@ -3,22 +3,22 @@ const toggle = document.querySelector('.toggle-btn');
 const toggleIcon = document.querySelector('.toggle-btn i');
 const dropDown = document.querySelector('.drop-down');
 
+console.log(dropDown);
+
+
 window.addEventListener('scroll', function() {
     if (window.scrollY > 300) {
         nav.classList.add('nav-scroll');
-        dropDown.style.setProperty('background','rgba(0, 0, 0, .15);');
+        dropDown.style.setProperty('background','rgba(0, 0, 0, .15)');
     } else if (window.scrollY <= 300){
         nav.classList.remove('nav-scroll');
-        dropDown.style.setProperty('background','rgba(255, 255, 255, .15);');
+        dropDown.style.setProperty('background','rgba(255, 255, 255, .15)');
     }
 });
 
-
-
-toggle.onClick = function() {
-    dropDown.classList.add('open');
+toggle.addEventListener('click', function() {
+    dropDown.classList.toggle('open');
     const isOpen = dropDown.classList.contains('open');
-
+    console.log(isOpen);
     toggleIcon.classList = isOpen ? 'fas fa-xmark' : 'fas fa-bars';
-
-}
+});
